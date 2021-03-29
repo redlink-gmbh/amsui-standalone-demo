7a16c9f11e3e5ac684c0c117c86e05dcd22e1915
markusebner <markus.ebner@redlink.at>
2021-03-29 12:38:09 +0200

    add standalone usage demo of amsui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {SearchFieldConfig, SuggestionParameter} from '@redlink/amsui';
+import {delay} from 'rxjs/operators';
+import {Observable, of} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amsui-standalone-demo';
+  searchKeyword = '';
+  searchFieldConfig: SearchFieldConfig = {
+    asyncSuggestionDataProvider: this.fakeSearchSuggestions.bind(this),
+  };
+
+  constructor(private readonly translateService: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translateService.setDefaultLang('de');
+
+    // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translateService.use('en');
+  }
+
+  handleSearchEvent(searchKeyword: string): void {
+    this.searchKeyword = searchKeyword;
+    alert('You searched for ' + searchKeyword);
+  }
+
+  fakeSearchSuggestions(input: SuggestionParameter): Observable<string[]> {
+    const filteredSuggestions = [
+      'Knowledge',
+      'Knowledge management',
+      'Knowledge officer',
+      'My knowledge',
+      'Your knowledge',
+      'Your knowledge',
+      'Natural Language Processing',
+      'NLP',
+      'Management',
+      'Solr',
+      'Amsui',
+    ]
+      .filter((option) =>
+        option.toLowerCase().includes(input.keyword.toLowerCase())
+      )
+      .slice(0, input.numberOfSuggestions);
+    return of(filteredSuggestions).pipe(delay(300));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {IModuleTranslationOptions, ModuleTranslateLoader} from '@larscom/ngx-translate-module-loader';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {SearchFieldModule} from '@redlink/amsui';
+
+export function moduleHttpLoaderFactory(http: HttpClient): ModuleTranslateLoader {
+  const baseTranslateUrl = './assets/i18n';
+
+  const options: IModuleTranslationOptions = {
+    modules: [
+      { baseTranslateUrl: baseTranslateUrl + '/amsui' },
+      { baseTranslateUrl },
+    ],
+  };
+
+  return new ModuleTranslateLoader(http, options);
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: moduleHttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    SearchFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
