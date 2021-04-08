bdf20f1eec6b350f3623edc46840ab93edfef823
markusebner <markus.ebner@redlink.at>
2021-04-08 14:30:22 +0200

    use new version + demo adaptions

--- README.md
+++ README.md
@@ -7,9 +7,13 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
 ## [Demo](https://dev.redlink.io/amsui-standalone-demo/)
+
 ## [Documentation AMSUI](https://dev.redlink.io/amsui)
+
 ## [npm library](https://www.npmjs.com/package/@redlink/amsui)
+
 ## [AMSUI library on github](https://github.com/redlink-gmbh/amsui)
+
 ## Other demos
-* [Demo with Solr Index](https://dev.redlink.io/amsui-solr-demo/) 
+* [Demo with Public API](https://dev.redlink.io/amsui-public-api-demo/) 
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {SearchFieldConfig, SearchFieldText, SuggestionParameter} from '@redlink/amsui';
-import {delay} from 'rxjs/operators';
+import {catchError, map, timeout} from 'rxjs/operators';
 import {Observable, of} from 'rxjs';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +11,17 @@ import {Observable, of} from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'amsui-standalone-demo';
+  title = 'AMSUI standalone demo';
   searchKeyword = '';
   searchFieldConfig: SearchFieldConfig = {
-    asyncSuggestionDataProvider: this.fakeSearchSuggestions.bind(this),
+    asyncSuggestionDataProvider: this.searchSuggestions.bind(this),
   };
   searchFieldText: SearchFieldText = {
-    searchButtonText: 'Start search'
+    searchButtonText: 'Start search',
+    placeholderLabel: 'Search for authors'
   };
 
-  constructor(private readonly translateService: TranslateService) {
+  constructor(private readonly translateService: TranslateService, private readonly http: HttpClient) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translateService.setDefaultLang('de');
 
@@ -32,24 +34,21 @@ export class AppComponent {
     alert('You searched for ' + searchKeyword);
   }
 
-  fakeSearchSuggestions(input: SuggestionParameter): Observable<string[]> {
-    const filteredSuggestions = [
-      'Knowledge',
-      'Knowledge management',
-      'Knowledge officer',
-      'My knowledge',
-      'Your knowledge',
-      'Your knowledge',
-      'Natural Language Processing',
-      'NLP',
-      'Management',
-      'Solr',
-      'Amsui',
-    ]
-      .filter((option) =>
-        option.toLowerCase().includes(input.keyword.toLowerCase())
-      )
-      .slice(0, input.numberOfSuggestions);
-    return of(filteredSuggestions).pipe(delay(300));
+  searchSuggestions(input: SuggestionParameter): Observable<string[]> {
+    return this.http
+      .get<any>('https://openlibrary.org/search.json?facet=false&_spellcheck_count=0&fields=author_name&mode=everything', {
+          params: new HttpParams().set('q', input.keyword).set('limit', String(input.numberOfSuggestions)),
+      })
+      .pipe(
+        timeout(10000),
+        catchError(() => {
+          return of([]);
+        }),
+        map((response) => {
+          if (response.docs.length > 0) {
+            return response.docs.map((doc: any) => doc.author_name && doc.author_name.length > 0 ? doc.author_name[0] : 'No author name available');
+          }
+        })
+      );
   }
 }
